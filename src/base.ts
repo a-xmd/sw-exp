92a748a2328b10ad2e0ccf5e88419baf226f64c6
Ahmed Abbas <ahmed.abbas@nos.nl>
2024-05-25 13:38:32 +0200

    feat: experiment with param

--- src/base.ts
+++ src/base.ts
@@ -3,7 +3,7 @@ import './react-root'
 if ('serviceWorker' in navigator) {
   navigator.serviceWorker.register(
     // @ts-ignore
-    new URL('service-worker.ts', import.meta.url),
+    new URL('service-worker.ts?q=1', import.meta.url),
     { type: 'module' },
   )
 }
